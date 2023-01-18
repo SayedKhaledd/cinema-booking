568df656c6931af2f8dc8c6396ebfdace5c9460e
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2023-01-18 23:37:50 +0200

    init

--- src/test/java/com/example/spring/ApplicationTests.java
+++ src/test/java/com/example/spring/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Demo1ApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
