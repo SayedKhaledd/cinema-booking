99e236250de5dff48804b76f35fd10001458ddd5
SayedKhaledd <63265214+SayedKhaledd@users.noreply.github.com>
2023-01-19 01:07:38 +0200

    update

--- src/main/java/com/example/cinemabooking/CinemabookingApplication.java
+++ src/main/java/com/example/cinemabooking/CinemabookingApplication.java
@@ -15,6 +15,7 @@ public class CinemabookingApplication {
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
+
     }
 
 }
